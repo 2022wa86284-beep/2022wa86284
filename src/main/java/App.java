61cd80fa175a2b8546c7e5b3cf080b33a7a77286
2022wa86284-beep <2022wa86284@wilp.bits-pilani.ac.in>
2026-09-22 17:19:20 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,7 @@ public class App {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello 2022wa86284");
+        System.out.println("Continuos Integration successful");
 
     }
 
